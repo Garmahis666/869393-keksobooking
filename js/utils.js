60157e0e2e7a16f38cc89a90facf8565e7cbfd3a
Garmahis666 <garmahis@garmahis.ru>
2018-10-07 22:28:08 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -5,15 +5,15 @@
     return 0.5 - Math.random();
   };
 
-  window.randomSort = function (arrayToSort) {
-    return arrayToSort.sort(getRandom);
-  };
-
-  window.getFeatureFromClass = function (classText) {
-    return classText.split('--')[1];
-  };
-
-  window.getRandomValue = function (values) {
-    return values[Math.floor(Math.random() * values.length)];
+  window.utils = {
+    randomSort: function (arrayToSort) {
+      return arrayToSort.sort(getRandom);
+    },
+    getFeatureFromClass: function (classText) {
+      return classText.split('--')[1];
+    },
+    getRandomValue: function (values) {
+      return values[Math.floor(Math.random() * values.length)];
+    }
   };
 })();
