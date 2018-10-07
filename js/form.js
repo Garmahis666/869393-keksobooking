a303661edeaec6aa985ab2a1a36df80d5048f664
Garmahis666 <garmahis@garmahis.ru>
2018-10-07 17:01:08 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -8,6 +8,15 @@
   var timeout = document.querySelector('#timeout');
   var capacity = document.querySelector('#capacity');
   var roomNumber = document.querySelector('#room_number');
+  var typeOfHousing = document.querySelector('#type');
+  var costOfHousing = document.querySelector('#price');
+
+  var typesOfHousing = {
+    flat: {name: 'Квартира', minCost: 1000},
+    bungalo: {name: 'Бунгало', minCost: 0},
+    house: {name: 'Дом', minCost: 5000},
+    palace: {name: 'Дворец', minCost: 10000}
+  };
 
   var roomsCapacity = {
     '1': ['1'],
@@ -21,6 +30,10 @@
     FROM_DISABLE: 'ad-form--disabled'
   };
 
+  window.setAddress = function (addressText) {
+    address.value = addressText;
+  };
+
   window.onTimeinChange = function () {
     timeout.value = timein.value;
   };
@@ -30,10 +43,14 @@
   };
 
   window.onRoomNumberChange = function () {
-    window.roomNumberChange();
+    roomNumberChange();
   };
 
-  window.roomNumberChange = function () {
+  var setMinPrice = function () {
+    costOfHousing.min = typesOfHousing[typeOfHousing.value].minCost;
+  };
+
+  var roomNumberChange = function () {
     var capacityOptions = capacity.querySelectorAll('option');
     for (var i = 0; i < capacityOptions.length; i++) {
       capacityOptions[i].disabled = roomsCapacity[roomNumber.value].indexOf(capacityOptions[i].value) === -1;
@@ -48,7 +65,16 @@
     }
   };
 
-  window.enableForms = function () {
+  window.activateForm = function () {
+    enableForms();
+    roomNumberChange();
+    typeOfHousing.addEventListener('change', setMinPrice);
+    roomNumber.addEventListener('change', window.onRoomNumberChange);
+    timein.addEventListener('change', window.onTimeinChange);
+    timeout.addEventListener('change', window.onTimeoutChange);
+  };
+
+  var enableForms = function () {
     adForm.classList.remove(styleClasses.FROM_DISABLE);
     for (var i = 0; i < adFormElements.length; i++) {
       adFormElements[i].removeAttribute('disabled');
