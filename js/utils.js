0078f829acd8539db8752d331707424cb3f3d2fb
Garmahis666 <garmahis@garmahis.ru>
2018-10-07 17:02:03 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,12 +1,6 @@
 'use strict';
 
 (function () {
-  var mapPins = document.querySelector('.map__pins');
-  var randomSettings = {
-    MAIN_PIN_WIDTH: 65,
-    MAIN_PIN_HEIGHT: 87
-  };
-
   var getRandom = function () {
     return 0.5 - Math.random();
   };
@@ -21,28 +15,5 @@
 
   window.getRandomValue = function (values) {
     return values[Math.floor(Math.random() * values.length)];
-  };
-
-  var rangeOfCoords = {
-    minX: 0,
-    maxX: mapPins.offsetWidth - randomSettings.MAIN_PIN_WIDTH,
-    minY: 130 - randomSettings.MAIN_PIN_HEIGHT,
-    maxY: 630 - randomSettings.MAIN_PIN_HEIGHT
-  };
-
-  window.checkCoords = function (x, y) {
-    if (x < rangeOfCoords.minX) {
-      x = rangeOfCoords.minX;
-    }
-    if (y < rangeOfCoords.minY) {
-      y = rangeOfCoords.minY;
-    }
-    if (x > rangeOfCoords.maxX) {
-      x = rangeOfCoords.maxX;
-    }
-    if (y > rangeOfCoords.maxY) {
-      y = rangeOfCoords.maxY;
-    }
-    return [x, y];
   };
 })();
