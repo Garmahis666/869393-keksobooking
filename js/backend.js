9c7ea540e20824a6a7714046fec60f6bfc9e45cd
Garmahis666 <garmahis@garmahis.ru>
2018-10-09 10:40:46 +0300

    Update backend.js

--- js/backend.js
+++ js/backend.js
@@ -3,12 +3,21 @@
 (function () {
   var URL_UPLOAD = 'https://js.dump.academy/keksobooking';
   var URL_LOAD = 'https://js.dump.academy/keksobooking/data';
+  var TIMEOUT = 10000;
+  var OK_CODE = 200;
+
+  var sendRequest = function (xhr, url, type, data) {
+    xhr.responseType = 'json';
+    xhr.timeout = TIMEOUT;
+    xhr.open(type, url);
+    xhr.send(data);
+  };
+
   window.backend = {
     upload: function (data, onLoad, onError) {
       var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
       xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
+        if (xhr.status === OK_CODE) {
           onLoad();
         } else {
           onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
@@ -17,36 +26,27 @@
       xhr.addEventListener('error', function () {
         onError('Произошла ошибка соединения');
       });
-
       xhr.addEventListener('timeout', function () {
         onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
       });
-
-      xhr.open('POST', URL_UPLOAD);
-      xhr.send(data);
+      sendRequest(xhr, URL_UPLOAD, 'POST', data);
     },
     load: function (onSuccess, onError) {
       var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
       xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
+        if (xhr.status === OK_CODE) {
           onSuccess(xhr.response);
         } else {
           onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
         }
       });
-
       xhr.addEventListener('error', function () {
         onError('Произошла ошибка соединения');
       });
-
       xhr.addEventListener('timeout', function () {
         onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
       });
-
-      xhr.timeout = 10000;
-      xhr.open('GET', URL_LOAD);
-      xhr.send();
+      sendRequest(xhr, URL_LOAD, 'GET', null);
     }
   };
 })();
