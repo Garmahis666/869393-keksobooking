35d904f47531b867b84f59b094a984aa50048799
Garmahis666 <garmahis@garmahis.ru>
2018-10-09 22:26:17 +0300

    Update backend.js

--- js/backend.js
+++ js/backend.js
@@ -6,47 +6,36 @@
   var TIMEOUT = 10000;
   var OK_CODE = 200;
 
-  var sendRequest = function (xhr, url, type, data) {
+  var addEvents = function (xhr, onSuccess, onError) {
     xhr.responseType = 'json';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === OK_CODE) {
+        onSuccess(xhr.response);
+      } else {
+        onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
     xhr.timeout = TIMEOUT;
-    xhr.open(type, url);
-    xhr.send(data);
   };
 
   window.backend = {
     upload: function (data, onLoad, onError) {
       var xhr = new XMLHttpRequest();
-      xhr.addEventListener('load', function () {
-        if (xhr.status === OK_CODE) {
-          onLoad();
-        } else {
-          onError('Статус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-      sendRequest(xhr, URL_UPLOAD, 'POST', data);
+      addEvents(xhr, onLoad, onError);
+      xhr.open('POST', URL_UPLOAD);
+      xhr.send(data);
     },
     load: function (onSuccess, onError) {
       var xhr = new XMLHttpRequest();
-      xhr.addEventListener('load', function () {
-        if (xhr.status === OK_CODE) {
-          onSuccess(xhr.response);
-        } else {
-          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
-      sendRequest(xhr, URL_LOAD, 'GET', null);
+      addEvents(xhr, onSuccess, onError);
+      xhr.open('GET', URL_LOAD);
+      xhr.send();
     }
   };
 })();
