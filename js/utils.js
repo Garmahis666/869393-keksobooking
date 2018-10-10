d1c675771e57ac7a0dc9cb1b8b4bb2fa991e9ca7
Garmahis666 <garmahis@garmahis.ru>
2018-10-10 18:17:03 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -30,35 +30,3 @@
     }
   };
 })();
-'use strict';
-
-(function () {
-  var getRandom = function () {
-    return 0.5 - Math.random();
-  };
-
-  window.utils = {
-    randomSort: function (arrayToSort) {
-      return arrayToSort.sort(getRandom);
-    },
-    getFeatureFromClass: function (classText) {
-      return classText.split('--')[1];
-    },
-    getRandomValue: function (values) {
-      return values[Math.floor(Math.random() * values.length)];
-    },
-    debounce: function (fun, interval) {
-      var lastTimeout = null;
-
-      return function () {
-        var args = arguments;
-        if (lastTimeout) {
-          window.clearTimeout(lastTimeout);
-        }
-        lastTimeout = window.setTimeout(function () {
-          fun.apply(null, args);
-        }, interval);
-      };
-    }
-  };
-})();
