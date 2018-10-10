0ad094538d9a5869f7728984955f61dabbf623d6
Garmahis666 <garmahis@garmahis.ru>
2018-10-10 22:45:40 +0300

    Update form.js

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 'use strict';
 
 (function () {
+  var OK_TIMEOUT = 2000;
+  var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
   var adForm = document.querySelector('.ad-form');
   var adFormElements = adForm.querySelectorAll('fieldset');
   var address = document.querySelector('#address');
@@ -11,8 +14,14 @@
   var typeOfHousing = document.querySelector('#type');
   var costOfHousing = document.querySelector('#price');
   var errorTemplate = document.querySelector('#error').content.querySelector('.error');
-  //  var successTemplate = document.querySelector('#success').content.querySelector('.success');
+  var successTemplate = document.querySelector('#success').content.querySelector('.success');
   var main = document.querySelector('main');
+  var avatarChooser = document.querySelector('.ad-form__field input[type=file]');
+  var avatar = document.querySelector('.ad-form-header__preview img');
+  var emptyAvatarImage = 'img/muffin-grey.svg';
+  var clearFormButton = document.querySelector('.ad-form__reset');
+  var fotoChooser = document.querySelector('.ad-form__upload input[type=file]');
+  var fotoBox = document.querySelector('.ad-form__photo-container');
 
   var typesOfHousing = {
     flat: {name: 'Квартира', minCost: 1000},
@@ -60,13 +69,21 @@
   var onUploadForm = function () {
     disableFrom();
     window.map.deactivate();
-    // var newMessage = successTemplate.cloneNode(true);
-    // main.appendChild(newMessage);
+    var newMessage = successTemplate.cloneNode(true);
+    main.appendChild(newMessage);
+    (window.utils.debounce(function () {
+      main.removeChild(newMessage);
+    }, OK_TIMEOUT))();
   };
 
   var onUploadFormError = function (message) {
     var newMessage = errorTemplate.cloneNode(true);
     newMessage.querySelector('p').innerText = message;
+    var button = newMessage.querySelector('.error__button');
+    button.addEventListener('click', function () {
+      main.removeChild(newMessage);
+      window.backend.upload(new FormData(adForm), onUploadForm, onUploadFormError);
+    });
     main.appendChild(newMessage);
   };
 
@@ -77,14 +94,28 @@
     }
     address.readOnly = true;
     roomNumberChange();
+    clearFormButton.addEventListener('click', clearAllPhoto);
     typeOfHousing.addEventListener('change', setMinPrice);
     roomNumber.addEventListener('change', onRoomNumberChange);
     timein.addEventListener('change', onTimeinChange);
     timeout.addEventListener('change', onTimeoutChange);
+    avatarChooser.addEventListener('change', onChangeAvatar);
+    fotoChooser.addEventListener('change', onAddPhoto);
     adForm.addEventListener('submit', function (evt) {
       window.backend.upload(new FormData(adForm), onUploadForm, onUploadFormError);
       evt.preventDefault();
     });
+  };
+
+  var clearAllPhoto = function () {
+    clearAvatar();
+    clearPhotos();
+  };
+
+  var createPhotoContainer = function () {
+    var divContainer = document.createElement('div');
+    divContainer.classList.add('ad-form__photo');
+    return divContainer;
   };
 
   var disableFrom = function () {
@@ -93,10 +124,67 @@
     for (var i = 0; i < adFormElements.length; i++) {
       adFormElements[i].setAttribute('disabled', 'disabled');
     }
+    clearAllPhoto();
     typeOfHousing.removeEventListener('change', setMinPrice);
     roomNumber.removeEventListener('change', onRoomNumberChange);
     timein.removeEventListener('change', onTimeinChange);
     timeout.removeEventListener('change', onTimeoutChange);
+    avatarChooser.removeEventListener('change', onChangeAvatar);
+    clearFormButton.removeEventListener('click', clearAllPhoto);
+    fotoChooser.removeEventListener('change', onAddPhoto);
+  };
+
+  var onChangeAvatar = function () {
+    var file = avatarChooser.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        avatar.src = reader.result;
+      });
+      reader.readAsDataURL(file);
+    }
+  };
+
+  var clearAvatar = function () {
+    avatar.src = emptyAvatarImage;
+  };
+
+  var clearPhotos = function () {
+    var elements = fotoBox.querySelectorAll('.ad-form__photo');
+    if (elements) {
+      Array.prototype.forEach.call(elements, function (node) {
+        node.parentNode.removeChild(node);
+      });
+    }
+  };
+
+  var createPhoto = function (photo) {
+    var divContainer = createPhotoContainer();
+    var photoContainer = document.createElement('img');
+    photoContainer.style.width = '100%';
+    photoContainer.style.height = '100%';
+    photoContainer.src = photo;
+    divContainer.appendChild(photoContainer);
+    return divContainer;
+  };
+
+  var onAddPhoto = function () {
+    var file = fotoChooser.files[0];
+    var fileName = file.name.toLowerCase();
+    var matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+    if (matches) {
+      var reader = new FileReader();
+      reader.addEventListener('load', function () {
+        fotoBox.appendChild(createPhoto(reader.result));
+      });
+      reader.readAsDataURL(file);
+    }
   };
 
   window.form = {
