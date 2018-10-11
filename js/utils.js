1419ae9a61647b179951cc6a526e349fa50dbca3
Garmahis666 <garmahis@garmahis.ru>
2018-10-11 18:31:16 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,19 +1,9 @@
 'use strict';
 
 (function () {
-  var getRandom = function () {
-    return 0.5 - Math.random();
-  };
-
   window.utils = {
-    randomSort: function (arrayToSort) {
-      return arrayToSort.sort(getRandom);
-    },
     getFeatureFromClass: function (classText) {
       return classText.split('--')[1];
-    },
-    getRandomValue: function (values) {
-      return values[Math.floor(Math.random() * values.length)];
     },
     debounce: function (fun, interval) {
       var lastTimeout = null;
